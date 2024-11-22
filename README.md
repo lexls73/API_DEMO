6f8c54536861e3edef0cb0ef5cc3af1e85121d44
Alejandro Lopez Stanley <alejandro.lopez.ext@aizon.ai>
2024-11-22 14:31:02 -0300

    Impement GET method & save on json, csv & excel

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Breve descripción del proyecto y su propósito.
 
 
    
-2. 
+2. python api_client.py --api "unify" --customer "appdev" --method "GET" --entity_type "elements" --entity_id "IPS_Base_flow_rate" --endpoint "values" --startDate  "2024-11-19T00:00:55.000Z" --endDate "2024-11-19T00:01:55.000Z"
 3. 
 4. 
 5. Instalar venv: python -m venv .venv 
--- api_client.py
+++ api_client.py
@@ -2,6 +2,8 @@ import requests
 import configparser
 import json
 import argparse
+import pandas as pd
+import os
 
 class APIClient:
     
@@ -9,6 +11,8 @@ class APIClient:
     m_api:str
     m_api_key:str
     m_status:bool
+    m_archivo:str
+    m_folder_path = "./DATA/"
     
     def __init__(self, api='unify', customer='default'):
         """
@@ -20,6 +24,8 @@ class APIClient:
         """
         self.m_customer = customer
         self.m_api = api
+        print("====================================")
+        print("Iniciando la API")
         
         self.get_token()
 
@@ -27,6 +33,9 @@ class APIClient:
         if self.m_api == 'unify':
 
             payload = json.dumps(self.get_credentials())
+            
+            print("====================================")
+            print(f"Obteniendo el TOKEN para UNIFY - Customer {self.m_customer}")
 
             reqUrl = f"https://api.{self.m_customer}.bigfinite.ai/v2/token"
 
@@ -34,36 +43,80 @@ class APIClient:
                 "Content-Type": "application/json" 
             }
 
-
         response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
         
         if response.status_code in [200,203]:
             self.m_status = True
             data = response.json()
             self.m_api_key = data['accessToken']
-            print('connected')
+            print("====================================")
+            print('Conectado - token obtenido')
         else:
             self.m_status = False
+            print("============ERROR=============")
             print(data['message'])
 
     def get_credentials(self) -> dict:
         # Crear un objeto ConfigParser
         config = configparser.ConfigParser(interpolation=None)
-
-        # Leer el archivo de configuración
-        config.read('/Users/alejandro.lopez.ext/.config/APIClient/apiClientCredentials.cfg')
         
         if self.m_api == 'unify':
+            print("====================================")
+            print(f"Obteniendo Credenciales para entorno {self.m_customer}")
+
+            # Leer el archivo de configuración
+            config.read('/Users/alejandro.lopez.ext/.config/APIClient/apiClientCredentials.cfg')
+
             credentials = {
                 'username': config[self.m_customer]['user'],
                 'password': config[self.m_customer]['password'],
                 'customer': config[self.m_customer]['customer']
             }
-            
+
             self.m_customer = config[self.m_customer]['customer']
-        
+
+            print("====================================")
+            print(f"Credenciales para usuario {config[self.m_customer]['user']} Obtenidas")
+        else:
+            print("====================================")
+            print(f"Obteniendo Credenciales para entorno {self.m_customer}")
+            
         return credentials
+
+    def create_json(self, datos_json):
         
+        print("====================================")
+        print("Borrando archivos antiguos")
+        for filename in os.listdir(self.m_folder_path):
+            file_path = os.path.join(self.m_folder_path, filename)
+            if os.path.isfile(file_path):  # Comprobar si es un archivo
+                os.remove(file_path)  # Eliminar el archivo
+
+        print("====================================")
+        print("Archivos borrados")
+
+        # Guardar el JSON en un archivo
+        file = f"{self.m_folder_path}{self.m_archivo}.json"
+        with open(file, "w", encoding="utf-8") as archivo:
+            json.dump(datos_json, archivo, ensure_ascii=False, indent=4)
+
+        print("====================================")
+        print('Archivo JSON Creado')
+        self.create_files(file)
+
+    def create_files(self,file):
+
+        df = pd.read_json(file)
+        
+        #file_sin_extension = file.replace(".json", "")
+
+        df.to_excel(f"{self.m_folder_path}{self.m_archivo}.xlsx", index=True)
+        print("====================================")
+        print('Archivo Excel Creado')
+        df.to_csv(f"{self.m_folder_path}{self.m_archivo}.csv", index=True)
+        print("====================================")
+        print('Archivo csv Creado')
+
     def _make_request(self, method, endpoint, params=None, data=None, json=None):
         """
         Método interno para realizar solicitudes HTTP.
@@ -76,29 +129,36 @@ class APIClient:
         :return: Respuesta de la API.
         """
         
-        headersList = {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + self.m_api_key
-            }
-        
-        try:
-            response = requests.request(
-                method=method,
-                url=endpoint,
-                headers=headersList,
-                params=params,
-                data=data,
-                json=json
-            )
-            response.raise_for_status()  # Lanza una excepción si hay un error HTTP
-            return response.json()  # Devuelve el cuerpo de la respuesta en formato JSON
-        except requests.exceptions.RequestException as e:
-            print(f"Error en la solicitud: {e}")
-            return None
+        if self.m_status:
+            headersList = {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + self.m_api_key
+                }
+
+            try:
+                response = requests.request(
+                    method=method,
+                    url=endpoint,
+                    headers=headersList,
+                    params=params,
+                    data=data,
+                    json=json
+                )
+                response.raise_for_status()  # Lanza una excepción si hay un error HTTP
+                
+                self.create_json(response.json()['_embedded']['items'])
+                print("====================================")
+                print("Datos Obtenidos")
+                return True
+            except requests.exceptions.RequestException as e:
+                print("============ERROR=============")
+                print(f"Error en la solicitud: {e}")
+                return False
 
     def get(self, entity_type, entity_id, endpoint, params=None):
         """Realiza una solicitud GET."""
         
+        self.m_archivo = entity_id
         if self.m_api == 'unify':
             url = f"https://api.{self.m_customer}.bigfinite.ai/v2/{entity_type}/{entity_id}/{endpoint}"
         
@@ -148,6 +208,9 @@ if __name__ == "__main__":
     try:
         if args.method == "GET":
             data = client.get('elements','IPS_Base_flow_rate','values',params)
-        print("Datos obtenidos de la API:", data)
+        print("====================================")
+        print("Datos obtenidos de la API y guardados como JSON, XLSX y CSV")
+        print("====================================")
     except requests.exceptions.RequestException as e:
+        print("============ERROR=============")
         print(f"Error al obtener datos: {e}")
